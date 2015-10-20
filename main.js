50b36451c8f900d6facf4d8be09d4af884f62276
Jessey <jesseyeagan@Jesseys-MacBook-Air.local>
2015-10-20 08:41:41 -0600

    Monday Night

--- main.js
+++ main.js
@@ -212,6 +212,9 @@ $scope.submitProject = function() {
 
 	
 }
+
+
+
 $scope.open = function (index) {
 	$scope.selectedCraft = $scope.crafts[index]
 	console.log($scope.selectedCraft)
@@ -226,28 +229,31 @@ $scope.open = function (index) {
       }
 
     });
+  //   $scope.cancel = function () {
+  //   $modalInstance.dismiss('close');
+  // };
 }
 
 
-$scope.open = function (index) {
-	$scope.selectedCraft = $scope.crafts[index]
-	console.log($scope.selectedCraft)
-    var modalInstance = $modal.open({
-      animation: $scope.animationsEnabled,
-      templateUrl: 'logInModal',
-      controller: 'ModuleController',
-      resolve: {
-        items: function () {
-          return $scope.selectedCraft;
-        }
-      }
+// $scope.open = function (index) {
+// 	$scope.selectedCraft = $scope.crafts[index]
+// 	console.log($scope.selectedCraft)
+//     var modalInstance = $modal.open({
+//       animation: $scope.animationsEnabled,
+//       templateUrl: 'logInModal',
+//       controller: 'ModuleController',
+//       resolve: {
+//         items: function () {
+//           return $scope.selectedCraft;
+//         }
+//       }
 
-    });
-}
+//     });
+// }
 }])
 
 
-angular.module('craftApp').controller('ModuleController', ['$scope', '$modalInstance', 'items', function($scope, $modalInstance, items) {
+angular.module('ui.bootstrap').controller('ModuleController', ['$scope', '$modalInstance', 'items', function($scope, $modalInstance, items) {
 	$scope.selectedCraft = items;
 
 
@@ -255,6 +261,9 @@ angular.module('craftApp').controller('ModuleController', ['$scope', '$modalInst
 $scope.close = function () {
 	$modalInstance.close($scope.selectedCraft);
 };
+// $scope.cancel = function () {
+//     $modalInstance.dismiss('close');
+//   };
 	
 }])
 
